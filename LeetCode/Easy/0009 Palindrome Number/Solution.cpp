8ea84ba8c24ef2b040d5076b0ddb7aff8948bc45
Aman Gupta <aman7437gupta@gmail.com>
2026-07-28 00:25:52 +0530

    Add solution for LeetCode - Palindrome Number

--- LeetCode/Easy/0009 Palindrome Number/Solution.cpp
+++ LeetCode/Easy/0009 Palindrome Number/Solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int rev=0;
+        long long rev=0;
         int o=x;
         if(x < 0)
             return false;
         while(x>0){
-        long lorev=rev*10+x%10;
+         rev=rev*10+x%10;
         x=x/10;
         }
         if(o==rev){
