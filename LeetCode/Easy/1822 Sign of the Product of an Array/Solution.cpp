8ca49c1afc5bdf5b77c09526a0d6891b66281c55
Aman Gupta <aman7437gupta@gmail.com>
2026-08-14 22:56:59 +0530

    Add solution for LeetCode - Sign of the Product of an Array

--- LeetCode/Easy/1822 Sign of the Product of an Array/Solution.cpp
+++ LeetCode/Easy/1822 Sign of the Product of an Array/Solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int mult=1;
+        long loint mult=1;
         int ans;
         for(int i=0;i<nums.size();i++){
             mult=mult*nums[i];
