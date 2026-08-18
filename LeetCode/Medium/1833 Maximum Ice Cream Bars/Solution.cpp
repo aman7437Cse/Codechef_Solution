35864db14e100ee5bee7e9917a01f104a49d74a2
Aman Gupta <aman7437gupta@gmail.com>
2026-08-19 00:34:04 +0530

    Add solution for LeetCode - Maximum Ice Cream Bars

--- LeetCode/Medium/1833 Maximum Ice Cream Bars/Solution.cpp
+++ LeetCode/Medium/1833 Maximum Ice Cream Bars/Solution.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
         int sum=0;
         int count=0;
         for(int i=0;i<costs.size();i++){
