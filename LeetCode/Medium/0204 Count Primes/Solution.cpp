29a1fcacf2f35ba6bbcbc36c515e6093bae1a711
Aman Gupta <aman7437gupta@gmail.com>
2026-08-24 11:09:03 +0530

    Add solution for LeetCode - Count Primes

--- LeetCode/Medium/0204 Count Primes/Solution.cpp
+++ LeetCode/Medium/0204 Count Primes/Solution.cpp
@@ -5,7 +5,7 @@ public:
         
         int primecount=0;
 
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=n;i++){
            int count=0; 
         for(int j=1;j<=i;j++){
             if(i%j==0){
