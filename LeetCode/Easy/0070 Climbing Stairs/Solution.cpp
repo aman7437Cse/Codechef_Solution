70cac469af17876348c7487442ac2c9dbce37aa8
Aman Gupta <aman7437gupta@gmail.com>
2026-08-25 22:07:43 +0530

    Add solution for LeetCode - Climbing Stairs

--- LeetCode/Easy/0070 Climbing Stairs/Solution.cpp
+++ LeetCode/Easy/0070 Climbing Stairs/Solution.cpp
@@ -3,6 +3,12 @@ public:
     int climbStairs(int n) {
         if (n==1 || n==2)
         return n;
-        
+
+        int prev2=1;
+        int prev1=2;
+
+        for(int i=3;i<=n;i++){
+            int current=pr
+        }
     }
 };
