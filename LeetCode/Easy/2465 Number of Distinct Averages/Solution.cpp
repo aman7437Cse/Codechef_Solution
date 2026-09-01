f1f828849d3786064dcf9c26cc555c7b65b248a2
Aman Gupta <aman7437gupta@gmail.com>
2026-09-01 16:20:23 +0530

    Add solution for LeetCode - Number of Distinct Averages

---
+++ LeetCode/Easy/2465 Number of Distinct Averages/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+        vector<double> v;
+        sort(nums.begin(),nums.end());
+
+
+        int s=0;
+        int l=nums.size()-1;
+        while(l>=s){
+            double avg=(nums[l]+nums[s])/2.0;
+            v.push_back(avg);
+            s++;
+            l--;
+
+        }
+        v.erase(unique(v.begin(),v.end()),v.end());
+        
+        return v.size();
+        
+    }
+};
