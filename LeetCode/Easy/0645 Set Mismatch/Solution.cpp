307ab2634e1daaf4d0c222fddd0ee400961af3ac
Aman Gupta <aman7437gupta@gmail.com>
2026-09-02 10:55:01 +0530

    Add solution for LeetCode - Set Mismatch

---
+++ LeetCode/Easy/0645 Set Mismatch/Solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> v;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                v.push_back(nums[i]);
+                v.push_back(nums[i]+1);
+            }
+        }
+        return v;
+    }
+};
