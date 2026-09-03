b7ed06189046da36a55e1e4f40d6b4de61aec7fb
Aman Gupta <aman7437gupta@gmail.com>
2026-09-03 09:56:49 +0530

    Add solution for LeetCode - Split Array by Prime Indices

--- LeetCode/Medium/3618 Split Array by Prime Indices/Solution.cpp
+++ LeetCode/Medium/3618 Split Array by Prime Indices/Solution.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
 
-bool prime(int n){
+    bool prime(int n){
     int count=0;
     for(int i=1;i<=n;i++){
         if(n%i==0){
             count++;
         }
-        
-        
-        
     }
     if(count==2){
             return true;
+    }
     return false;
-}
+    }
     long long splitArray(vector<int>& nums) {
         
         
