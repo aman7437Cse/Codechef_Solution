e51787c8420e07d33d9af916b5bfe056646017cb
Aman Gupta <aman7437gupta@gmail.com>
2026-09-04 14:47:26 +0530

    Add solution for LeetCode - Minimum Add to Make Parentheses Valid

--- LeetCode/Medium/0921 Minimum Add to Make Parentheses Valid/Solution.cpp
+++ LeetCode/Medium/0921 Minimum Add to Make Parentheses Valid/Solution.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int count=0;
+        int sum=0;
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
                 count++;
             }
             else if(s[i]==')'){
-                count--;
+                sum++;
             }
         }
-        return abs(count);
+        return abs(sum-count);
         
     }
 };
