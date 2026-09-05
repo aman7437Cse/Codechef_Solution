d32419ce91b3f985ec96abde92f2658f198af554
Aman Gupta <aman7437gupta@gmail.com>
2026-09-05 20:40:52 +0530

    Add solution for LeetCode - Backspace String Compare

--- LeetCode/Easy/0844 Backspace String Compare/Solution.cpp
+++ LeetCode/Easy/0844 Backspace String Compare/Solution.cpp
@@ -1,21 +1,27 @@
-        }
-            else{
-                st.push(a);
-            }
-        for(char b:t){
-            if(b=='#'){
-                bt.pop();
-            }
-            else{
-                if(!bt.empty())
-                bt.push(b);
-            }
-        }
-        return st==bt;
-        
-    }
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>st;
+        stack<char>bt;
+        for(char a:s){
+            if(a=='#'){
+                if(!st.empty())
+                st.pop();
+            }
+            else{
+                st.push(a);
+            }
+        }
+        for(char b:t){
+            if(b=='#'){
+                if(!bt.empty())
+                bt.pop();
+            }
+            else{
+                bt.push(b);
+            }
+        }
+        return st==bt;
+        
+    }
 };
-            }
-                st.pop();
-                if(!st.empty())
-            if(a=='#'){
